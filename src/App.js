62efa5ae15c1dc4599b251b2b6dd485c2ea6c4d0
Taras Shulhan <taras.shulhan@gmail.com>
2021-10-08 18:10:14 +0300

    passing all tests

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import Character from './components/Character';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [characters, setCharacters] = useState(null);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+      .then(res => {
+        setCharacters(res.data);
+      }).catch(err => {
+        console.error(err);
+      })
+  },[])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters && characters.map(char => {
+        return <Character name = {char.name} birth_year = {char.birth_year}/>
+      })
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,43 @@
 // Write your Character component here
+import React from 'react';
+import styled, {keyframes} from 'styled-components';
+
+const kf = keyframes`
+  50% {
+    transform: scale(0.8);
+  }
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }
+`
+
+const StyledCharacter = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-left: 10%;
+  margin-right 10%;
+
+  transform: scale(2); //-> start of animation
+  opacity: 0; //-> start of animation
+  animation: ${kf} 0.4s ease-in-out forwards;
+`
+const StyledName = styled.p`
+
+`
+const StyledAge = styled.p`
+
+`
+
+const Character = props => {
+    const { name, birth_year } = props;
+  
+    return (
+      <StyledCharacter>
+        <StyledName>{name}</StyledName>
+        <StyledAge>{birth_year}</StyledAge>
+      </StyledCharacter>
+    );
+  };
+  
+  export default Character;
